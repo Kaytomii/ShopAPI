47af6eb144f5709ace90b1c3e200abbae679864e
Yan Tarasov <vzu3427@gmail.com>
2026-06-17 18:44:48 +0100

    Add project files.

---
+++ ShopApp/Controllers/ItemsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace ShopApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ItemsController:ControllerBase
+{
+    [HttpGet]
+    public string GetItem()
+    {
+        return "New Items";
+    }
+
+    [HttpGet("{id}")]
+    public string GetItemById([FromRoute] int id)
+    {
+        return $"Item id: {id}";
+    }
+}
---
+++ ShopApp/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopDomain.Models;
+
+namespace ShopApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController:ControllerBase
+    {
+        private List<Product> _products = new();
+        [HttpGet]
+        public List<Product> GetProducts()
+        {
+            _products.Add(new Product()
+            {
+                Title = "Milk",
+                Price = 44.6f
+            });
+
+            _products.Add(new Product()
+            {
+                Title = "Bread",
+                Price = 44.6f
+            });
+            return _products;
+
+        }
+    }
+}
